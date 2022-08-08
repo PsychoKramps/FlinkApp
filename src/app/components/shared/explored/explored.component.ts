fbad9b2c6b47c978d181e189c6498f492faa3cf3
CristianP50 <cristianjp.padierna2@gmail.com>
2022-08-08 15:13:40 -0500

    Se agregan rutas y se hace get de cursos

--- src/app/components/shared/explored/explored.component.ts
+++ src/app/components/shared/explored/explored.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ScriptsJsService } from 'src/app/components/services/scripts-js.service';
+import { ResentCursos } from '../../modules/cursos/models/resent-cursos';
+import { CursosService } from '../../modules/cursos/services/cursos.service';
 
 @Component({
   selector: 'app-explored',
@@ -8,8 +10,15 @@ import { ScriptsJsService } from 'src/app/components/services/scripts-js.service
 })
 export class ExploredComponent implements OnInit {
 
-  constructor( private _cargarScript:ScriptsJsService) {
+  recentCursos: ResentCursos[] = [];
+
+  constructor( private _cargarScript:ScriptsJsService, private cursosServices: CursosService) {
     _cargarScript.scripts(['isotope', 'owl-carousel', 'wow', 'tabs', 'popup', 'custom']);
+
+    this.cursosServices.getResentCursos()
+    .subscribe(cursos => {
+      this.recentCursos = cursos;
+    })
   }
 
   ngOnInit(): void {
